0febb619bb59bc84f1564ca16d29f8834e9bc71d
Kamil Polok <kamil.polokpl@gmail.com>
2019-07-15 12:33:26 +0200

    Added .json file for gift.

--- apis/gift/__init__.py
+++ apis/gift/__init__.py
@@ -5,8 +5,11 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from .namespace1 import api as ns1
 from .namepsace2 import api as ns2
 
-blueprint1 = Blueprint('blueprint1', __name__)
-get_swaggerui_blueprint()
-api1 = Api(blueprint1)
-api1.add_namespace(ns1, path='/prefix/of/ns1')
-api1.add_namespace(ns2)
+SWAGGER_URL = '/events/gift'
+API_URL = '/static/gift.json'
+
+gift_blueprint = get_swaggerui_blueprint(SWAGGER_URL,API_URL, cofig={'app_name': 'web'})
+api = Api(gift_blueprint)
+
+# api1.add_namespace(ns1, path='/prefix/of/ns1')
+# api1.add_namespace(ns2)
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from apis.gift import gift_blueprint, SWAGGER_URL as gift_url
 from apis.mobile_app import blueprint2 as api2
 from apis.web_app import blueprint1 as api1
 
+
 app = Flask(__name__)
+app.register_blueprint(gift_blueprint, url_prefix=gift_url)
 app.register_blueprint(api1, url_prefix='/api1/1')
 app.register_blueprint(api2, url_prefix='/api2/1')
 
