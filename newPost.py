38a8f24476c38fe633e675f456485db5909d5616
johntwardo <ineedip@gmail.com>
2019-07-15 16:08:08 +0200

    small changes

---
+++ newPost.py
@@ -0,0 +1,13 @@
+def incomingPost(joiobject):
+    import json
+    ola=[]
+    ola[0]='akuy j. iuy . uyi'
+    ola[1]='u y.oiy u.u j.'
+    def platformName(ola):
+        print('wu i')
+    def branchName():
+        if 'a'=='b':
+            print('aa')
+    def eventName(u):
+        if u.find in list==-1:
+            print(joiobject)
