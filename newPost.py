dbcbc9463b46136df73b9b91647df6b0e889c507
johntwardo <ineedip@gmail.com>
2019-07-16 12:32:58 +0200

    small changes

--- newPost.py
+++ newPost.py
@@ -1,13 +1,47 @@
-def incomingPost(joiobject):
+def incomingPost(jsonobject):
     import json
-    ola=[]
-    ola[0]='akuy j. iuy . uyi'
-    ola[1]='u y.oiy u.u j.'
-    def platformName(ola):
-        print('wu i')
-    def branchName():
-        if 'a'=='b':
-            print('aa')
-    def eventName(u):
-        if u.find in list==-1:
-            print(joiobject)
+    import os
+    parsejson=json.dumps(jsonobject)
+    parsed = json.loads(parsejson)
+    #print(parsed)
+    ola = parsed['platform']#'mobile'
+    ala = parsed['branch']#'staging'
+    print(ola)
+    print(ala)
+
+    def platform_name(igllo):
+        if igllo == 'mobile':
+            print("it's a mobile post")
+            path = '/mobile'
+        elif igllo == 'server':
+            print("it's a server/post")
+            path = '/server'
+        elif igllo == 'web':
+            print("it's a web post")
+            path = '/web'
+        elif igllo == 'gift':
+            print("it's a gift post")
+            path = '/gift'
+        else:
+            print("Unvalid platform name")
+            raise ValueError
+        branch_name(ala, path)
+
+    def branch_name(ollgi, path):
+        if ollgi == 'master':
+            print('master branch')
+            path = path + '/master'
+        elif ollgi == 'staging':
+            print('staging branch')
+            path = path + '/staging'
+        else:
+            print("Wrong branch name")
+            raise ValueError
+        print(path)
+        return (path)
+
+    platform_name(ola)
+
+
+#openair = '{"branch":"master","joiobject":"arguments","platform":"mobile"}'
+#incomingPost(openair)
