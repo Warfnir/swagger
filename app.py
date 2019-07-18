b9e09b8f2c4055c459089610fc1514d9e2f0bb1f
Kamil Polok <kamil.polokpl@gmail.com>
2019-07-18 09:30:46 +0200

    Deploying on heroku.

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
+import os
+
 from flask import Flask, request
 from apis.gift import gift_blueprint, SWAGGER_URL as gift_url
 from apis.mobile_app import mobile_blueprint, SWAGGER_URL as mobile_url
 from apis.web_app import web_blueprint, SWAGGER_URL as web_url
 from apis.server import server_blueprint, SWAGGER_URL as server_ur;
 from newPost import incomingPost
-#import core.jsonmodyfier as jmod
+# import core.jsonmodyfier as jmod
 import json
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.debug = True
 app.register_blueprint(gift_blueprint, url_prefix=gift_url)
 app.register_blueprint(mobile_blueprint, url_prefix=mobile_url)
 app.register_blueprint(web_blueprint, url_prefix=web_url)
-app.register_blueprint(server_blueprint,url_prefix=server_ur)
+app.register_blueprint(server_blueprint, url_prefix=server_ur)
 
-@app.route('/allevents',methods=['POST'])
+
+@app.route('/allevents', methods=['POST'])
 def parse_request():
     data = request.get_json()
     incomingPost(data)
     return "done"
-app.run(debug=True)
+
+
